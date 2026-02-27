62b355163093b78f33aea43eeb6a1b6a1b219ee3
Livia Alves <liviaalvesetec25@gmail.com>
2026-02-27 08:40:06 -0300

    Add files via upload

---
+++ aula02/exemplo06.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exemplo 05</title>
+</head>
+<body>
+    <?php
+    $a = 4;
+    $b = 0;
+    if ($b == 0) {
+        echo "Não foi possivel executar esse programa";
+    } else {
+        $r = $a / $b;
+
+        echo $r;
+    
+        echo "<br>";
+    }
+    
+
+   
+    
+
+    ?>
+</body>
+</html>
