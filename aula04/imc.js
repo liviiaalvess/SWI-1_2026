dec3d88db6730e3da4cfa4e3c759bb5b40abb014
liviiaalvess <liviaalvesetec25@gmail.com>
2026-03-18 07:23:06 -0300

    bora la

--- aula04/imc.js
+++ aula04/imc.js
@@ -1,4 +1,18 @@
+function trocarNome() {
+    var nome = document.getElementById("campoNome").value;
+    document.getElementById("nomeExibido").innerText = 
+    "Nome : " + nome;
+}
+function somar(){
+    var numero1 = parseFloat(document.getElementById("num1").value);
 
+    var numero2 = parseFloat(document.getElementById("num2").value);
+
+    var soma = numero1 + numero2;
+
+    document.getElementById("resultado").innerText = 
+    "Resultado : "+ soma;
+}
 function dividir() {
     var numro1 = parseFloat(document.getElementById("valorpeso").value);
 
